9c4e413e1096713e963cea2fd96b3e4779fa67f4
bst82 <bt7355711982@gmail.com>
2024-06-02 16:52:16 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SliderComponent } from "./components/slider/slider.component";
+import { CardsComponent } from "./components/cards/cards.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, SliderComponent, CardsComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'flipkartclone';
